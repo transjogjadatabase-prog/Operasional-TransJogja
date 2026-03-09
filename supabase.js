472efad54afd39bd60d72f2c50c4714c779b99a8
transjogjadatabase-prog <transjogjadatabase@gmail.com>
2026-03-09 13:23:35 +0700

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -5,7 +5,7 @@
 
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm';
 
-const SUPABASE_URL      = 'https://XXXXXXXXXXXX.supabase.co';   // ← ganti
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...'; // ← ganti
+const SUPABASE_URL      = 'https://rzmeitgcbcpctisxsxpq.supabase.co';   // ← ganti
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ6bWVpdGdjYmNwY3Rpc3hzeHBxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMwMzU0NTIsImV4cCI6MjA4ODYxMTQ1Mn0.NJivuuKmq48in32Ruk5hcf5F3LbNa2jL8yjD8GVClj4'; // ← ganti
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
